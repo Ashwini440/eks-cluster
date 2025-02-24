db98a5dd8a1975c600e8f9835709cc2bb81c274e
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-02-24 10:20:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_container_cluster" "primary" {
+resource "google_container_cluster" "my-cluster" {
   name     = var.cluster_name
   location = var.region  # This is still the region, like 'us-central1'
 
