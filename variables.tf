bdb901fa26007c2a39f058516f9623908f49d67b
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-02-25 15:44:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,24 @@
 variable "aws_region" {
-  default = "us-east-1"
-  description = "aws region"
+  type = string
 }
+variable "aws_vpc" {
+  type = string
+}
+variable "eks_cluster_role" {
+  type = string
+}
+variable "eks_cluster_name" {
+  type = string
+}
+variable "eks_node_role" {
+  type = string
+}
+variable "eks_nodegroup_name" {
+  type = string
+}
+variable "access_key" {
+  type        = string
+ }
+ variable "secret_access_key" {
+   type        = string
+ } 
