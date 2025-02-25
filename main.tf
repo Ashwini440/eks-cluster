526a806ad767b2358dcf4610e8adc4225ca31838
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-02-25 15:46:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,7 +88,3 @@ resource "aws_eks_node_group" "eks_nodes" {
   ]
 }
 
-# Configure kubectl access
-output "kubeconfig_command" {
-  value = "aws eks update-kubeconfig --region us-east-1 --name ${aws_eks_cluster.eks.name}"
-}
