13110598363d59eeede756bc44ecfd16df1e40f5
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-02-25 15:47:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,28 +1,5 @@
-output "cluster_id" {
-  description = "EKS cluster ID."
-  value       = module.eks.cluster_id
+# Configure kubectl access
+output "kubeconfig_command" {
+  value = "aws eks update-kubeconfig --region us-east-1 --name ${aws_eks_cluster.eks.name}"
 }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
-}
-
-output "region" {
-  description = "AWS region"
-  value       = var.aws_region
-}
-
-output "oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
-}
-
-#output "zz_update_kubeconfig_command" {
-  # value = "aws eks update-kubeconfig --name " + module.eks.cluster_id
-#  value = format("%s %s %s %s", "aws eks update-kubeconfig --name", module.eks.cluster_id, "--region", var.aws_region)
-#}
