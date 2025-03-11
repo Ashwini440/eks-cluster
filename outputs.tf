d42ee145b3c9293d1f0af6c9d088ec9bd81571da
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-11 13:52:31 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "kubeconfig_command" {
   value = "aws eks update-kubeconfig --region us-east-1 --name ${aws_eks_cluster.eks.name}"
 }
 output "kubeconfig" {
-  value = aws_eks_cluster.marvel.kubeconfig
+  value = aws_eks_cluster.eks.kubeconfig
 }
