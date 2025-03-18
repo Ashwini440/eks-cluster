1955e72e83fc565b74dbea8db18c04919a22bc42
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-18 16:38:00 +0530

    Update delegator.tf

--- delegator.tf
+++ delegator.tf
@@ -1,19 +1,25 @@
+data "aws_caller_identity" "current" {}
+data "aws_eks_cluster" "eks" {
+  name = var.eks_cluster_name
+}
+
+
+
 provider "kubernetes" {
-  host                   = aws_eks_cluster.eks.endpoint
+  host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
-
-resource "null_resource" "install_harness_delegator" {
-  depends_on = [aws_eks_cluster.eks]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      export KUBECONFIG="${aws_eks_cluster.example.kubeconfig}"
-      kubectl apply -f https://app.harness.io/docs/delegator-install.yaml
-    EOT
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    token                  = data.aws_eks_cluster.eks.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   }
 }
+
+
+
 module "delegate" {
   source = "harness/harness-delegate/kubernetes"
   version = "0.1.8"
@@ -27,5 +33,6 @@ module "delegate" {
   delegate_image = "harness/delegate:25.02.85300"
   replicas = 1
   upgrader_enabled = true
+  depends_on = [aws_eks_cluster.eks]
 }
 
