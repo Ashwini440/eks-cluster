1aca6b6fae822fde27cd6f82311a59140334047a
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-04-02 10:40:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 # Configure kubectl access
 output "kubeconfig_command" {
-  value = "aws eks update-kubeconfig --region us-east-1 --name ${aws_eks_cluster.eks.name}"
+  value = "aws eks update-kubeconfig --region us-west-2 --name ${aws_eks_cluster.eks.name}"
 }
 output "clustername" {
  value = aws_eks_cluster.eks.name
