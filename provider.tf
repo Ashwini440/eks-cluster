fe4b192e94b77f73f9737da84c9c136ae1ed6fd7
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-04-15 12:06:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region = var.aws_region
 }
+provider "kubernetes" {
+  host                   = "https://${aws_eks_cluster.cluster.endpoint}"
+  cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.cert_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
